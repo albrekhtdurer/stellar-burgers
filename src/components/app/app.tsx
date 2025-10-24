839c603f71c4ba834b4d2325eae64db39d41befc
Ekaterina Novikova <fearnocait@gmail.com>
2025-10-24 11:49:03 +0300

    [feat] добавили базовые маршруты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,40 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import { Routes, Route } from 'react-router-dom';
 
 import { AppHeader } from '@components';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='*' element={<NotFound404 />} />
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      //TODO: protect
+      <Route path='/login' element={<Login />} />
+      //TODO: protect
+      <Route path='/register' element={<Register />} />
+      // TODO: protect
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      // TODO: protect
+      <Route path='/reset-password' element={<ResetPassword />} />
+      // TODO: protect
+      <Route path='/profile' element={<Profile />} />
+      // TODO: protect
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+    </Routes>
   </div>
 );
 
