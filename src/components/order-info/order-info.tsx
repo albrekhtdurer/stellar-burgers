1084d00b98d8ac8015414c37e982a8dc98677b3b
Ekaterina Novikova <fearnocait@gmail.com>
2025-10-28 11:17:27 +0300

    [feat] отрисовали модалку с данными заказа в feed

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -4,18 +4,14 @@ import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
 import { useSelector } from '../../services/store';
 import { ingredientsSelector } from '../../services/slices/burgersSlice';
+import { useParams } from 'react-router-dom';
+import { ordersSelector } from '../../services/slices/ordersSlice';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const { number } = useParams();
+  const orders = useSelector(ordersSelector);
+  console.log(orders[orders.length - 1]);
+  const orderData = orders.find((order) => order.number.toString() === number);
 
   const ingredients = useSelector(ingredientsSelector) || [];
 
