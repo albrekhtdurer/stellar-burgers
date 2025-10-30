1031d45ec58211a157181964234a14bf6764c8ea
Ekaterina Novikova <fearnocait@gmail.com>
2025-10-30 12:43:27 +0300

    [fix] добавили NavLink

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,7 +7,7 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
   const location = useLocation();
@@ -17,7 +17,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
     <header className={styles.header}>
       <nav className={`${styles.menu} p-4`}>
         <div className={styles.menu_part_left}>
-          <Link
+          <NavLink
             to='/'
             className={`${styles.link} ${currentPath == '/' ? styles.link_active : ''}`}
           >
@@ -25,8 +25,8 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
             <p className={`text text_type_main-default ml-2 mr-10`}>
               Конструктор
             </p>
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to='/feed'
             className={`${styles.link} ${currentPath == '/feed' ? styles.link_active : ''}`}
           >
@@ -34,13 +34,13 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
             <p className='text text_type_main-default ml-2 mr-10'>
               Лента заказов
             </p>
-          </Link>
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <Logo className='' />
         </div>
         <div className={styles.link_position_last}>
-          <Link
+          <NavLink
             to='/profile'
             className={`${styles.link} ${currentPath == '/profile' ? styles.link_active : ''}`}
           >
@@ -48,7 +48,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => {
             <p className='text text_type_main-default ml-2 mr-10'>
               {userName || 'Личный кабинет'}
             </p>
-          </Link>
+          </NavLink>
         </div>
       </nav>
     </header>
