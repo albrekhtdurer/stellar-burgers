8d543fc8d970f40413d77357d4fa7fefaf07c612
Ekaterina Novikova <fearnocait@gmail.com>
2025-11-12 13:50:27 +0300

    [feat] тесты для редьюсера burgerConstructor

--- src/services/constructor/constructor.mock.ts
+++ src/services/constructor/constructor.mock.ts
@@ -3,3 +3,274 @@ import { TConstructorState } from './slice';
 export const initialState: TConstructorState = {
   items: { bun: null, ingredients: [] }
 };
+
+export const defaultState: TConstructorState = {
+  items: {
+    bun: {
+      _id: '643d69a5c3f7b9001cfa093c',
+      name: 'Краторная булка N-200i',
+      type: 'bun',
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+      id: 'O3owvdiWO567dJiBBozOk'
+    },
+    ingredients: [
+      {
+        _id: '643d69a5c3f7b9001cfa0941',
+        name: 'Биокотлета из марсианской Магнолии',
+        type: 'main',
+        proteins: 420,
+        fat: 142,
+        carbohydrates: 242,
+        calories: 4242,
+        price: 424,
+        image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+        id: '-bBxRtS8eXT5SecidjOxh'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa093e',
+        name: 'Филе Люминесцентного тетраодонтимформа',
+        type: 'main',
+        proteins: 44,
+        fat: 26,
+        carbohydrates: 85,
+        calories: 643,
+        price: 988,
+        image: 'https://code.s3.yandex.net/react/code/meat-03.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-03-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-03-large.png',
+        id: 'FsHBXoU0ydjGgU1d--anq'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0940',
+        name: 'Говяжий метеорит (отбивная)',
+        type: 'main',
+        proteins: 800,
+        fat: 800,
+        carbohydrates: 300,
+        calories: 2674,
+        price: 3000,
+        image: 'https://code.s3.yandex.net/react/code/meat-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-04-large.png',
+        id: 'li6D9uuLKkrZDTpcUqsAY'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0948',
+        name: 'Кристаллы марсианских альфа-сахаридов',
+        type: 'main',
+        proteins: 234,
+        fat: 432,
+        carbohydrates: 111,
+        calories: 189,
+        price: 762,
+        image: 'https://code.s3.yandex.net/react/code/core.png',
+        image_mobile: 'https://code.s3.yandex.net/react/code/core-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/core-large.png',
+        id: 'PX4jQCJIjsElnWShYfcxv'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0943',
+        name: 'Соус фирменный Space Sauce',
+        type: 'sauce',
+        proteins: 50,
+        fat: 22,
+        carbohydrates: 11,
+        calories: 14,
+        price: 80,
+        image: 'https://code.s3.yandex.net/react/code/sauce-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/sauce-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/sauce-04-large.png',
+        id: 'Aad7VVTDi1IIiu7keJuNH'
+      }
+    ]
+  }
+};
+
+export const stateWithDeletedCrystals: TConstructorState = {
+  items: {
+    bun: {
+      _id: '643d69a5c3f7b9001cfa093c',
+      name: 'Краторная булка N-200i',
+      type: 'bun',
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+      id: 'O3owvdiWO567dJiBBozOk'
+    },
+    ingredients: [
+      {
+        _id: '643d69a5c3f7b9001cfa0941',
+        name: 'Биокотлета из марсианской Магнолии',
+        type: 'main',
+        proteins: 420,
+        fat: 142,
+        carbohydrates: 242,
+        calories: 4242,
+        price: 424,
+        image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+        id: '-bBxRtS8eXT5SecidjOxh'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa093e',
+        name: 'Филе Люминесцентного тетраодонтимформа',
+        type: 'main',
+        proteins: 44,
+        fat: 26,
+        carbohydrates: 85,
+        calories: 643,
+        price: 988,
+        image: 'https://code.s3.yandex.net/react/code/meat-03.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-03-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-03-large.png',
+        id: 'FsHBXoU0ydjGgU1d--anq'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0940',
+        name: 'Говяжий метеорит (отбивная)',
+        type: 'main',
+        proteins: 800,
+        fat: 800,
+        carbohydrates: 300,
+        calories: 2674,
+        price: 3000,
+        image: 'https://code.s3.yandex.net/react/code/meat-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-04-large.png',
+        id: 'li6D9uuLKkrZDTpcUqsAY'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0943',
+        name: 'Соус фирменный Space Sauce',
+        type: 'sauce',
+        proteins: 50,
+        fat: 22,
+        carbohydrates: 11,
+        calories: 14,
+        price: 80,
+        image: 'https://code.s3.yandex.net/react/code/sauce-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/sauce-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/sauce-04-large.png',
+        id: 'Aad7VVTDi1IIiu7keJuNH'
+      }
+    ]
+  }
+};
+
+export const stateWithCrystalsMovedUp: TConstructorState = {
+  items: {
+    bun: {
+      _id: '643d69a5c3f7b9001cfa093c',
+      name: 'Краторная булка N-200i',
+      type: 'bun',
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+      image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+      image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
+      id: 'O3owvdiWO567dJiBBozOk'
+    },
+    ingredients: [
+      {
+        _id: '643d69a5c3f7b9001cfa0941',
+        name: 'Биокотлета из марсианской Магнолии',
+        type: 'main',
+        proteins: 420,
+        fat: 142,
+        carbohydrates: 242,
+        calories: 4242,
+        price: 424,
+        image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png',
+        id: '-bBxRtS8eXT5SecidjOxh'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa093e',
+        name: 'Филе Люминесцентного тетраодонтимформа',
+        type: 'main',
+        proteins: 44,
+        fat: 26,
+        carbohydrates: 85,
+        calories: 643,
+        price: 988,
+        image: 'https://code.s3.yandex.net/react/code/meat-03.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-03-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-03-large.png',
+        id: 'FsHBXoU0ydjGgU1d--anq'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0948',
+        name: 'Кристаллы марсианских альфа-сахаридов',
+        type: 'main',
+        proteins: 234,
+        fat: 432,
+        carbohydrates: 111,
+        calories: 189,
+        price: 762,
+        image: 'https://code.s3.yandex.net/react/code/core.png',
+        image_mobile: 'https://code.s3.yandex.net/react/code/core-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/core-large.png',
+        id: 'PX4jQCJIjsElnWShYfcxv'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0940',
+        name: 'Говяжий метеорит (отбивная)',
+        type: 'main',
+        proteins: 800,
+        fat: 800,
+        carbohydrates: 300,
+        calories: 2674,
+        price: 3000,
+        image: 'https://code.s3.yandex.net/react/code/meat-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/meat-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/meat-04-large.png',
+        id: 'li6D9uuLKkrZDTpcUqsAY'
+      },
+      {
+        _id: '643d69a5c3f7b9001cfa0943',
+        name: 'Соус фирменный Space Sauce',
+        type: 'sauce',
+        proteins: 50,
+        fat: 22,
+        carbohydrates: 11,
+        calories: 14,
+        price: 80,
+        image: 'https://code.s3.yandex.net/react/code/sauce-04.png',
+        image_mobile:
+          'https://code.s3.yandex.net/react/code/sauce-04-mobile.png',
+        image_large: 'https://code.s3.yandex.net/react/code/sauce-04-large.png',
+        id: 'Aad7VVTDi1IIiu7keJuNH'
+      }
+    ]
+  }
+};
